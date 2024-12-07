f442e9ddd2c0ce9366677bca973299849ea45613
Sieg Apenas <MarceloPetitotR@gmail.com>
2024-12-07 08:53:15 -0300

    build: estrutura inicial de arquivos e primeiras cenas

---
+++ scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+@onready var play_button = $Control/Play
+@onready var options_button = $Control/Options
+@onready var credits_button = $Control/Credits
+
+func _ready():
+	pass
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
+	pass
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+	pass
+	
+
+func _on_credits_pressed():
+	#get_tree().change_scene_to_file()
+	pass
